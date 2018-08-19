ca177cc8a72c43644a78a0d2d9cdacc52f72294a
hexagonlabs <h.simon93@gmail.com>
2018-08-19 15:25:30 -0400

    Update RequestHelper.java

--- project2BackEnd/src/main/java/com/revature/servlet/RequestHelper.java
+++ project2BackEnd/src/main/java/com/revature/servlet/RequestHelper.java
@@ -4,24 +4,28 @@ import javax.servlet.http.HttpServletRequest;
 
 public class RequestHelper {
 
-	public static CharSequence process(HttpServletRequest request) {
-		System.out.println("boo");
-		return "
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Cucumber Features</title>
-    <link href="style.css" rel="stylesheet">
-    <script src="jquery-1.8.2.min.js"></script>
-    <script src="formatter.js"></script>
-    <script src="report.js"></script>
-  </head>
-  <body>
-    <div class="cucumber-report"></div>
-  </body>
-</html>
-";
+	public static String process(HttpServletRequest request) {
+		System.out.println("request processing:");
+		
+		/////////////////////////
+		////////////////////////////
+		
+		
+		////////////////////////////
+		System.out.println("Reading NGResults html from filesystem:");
+		String result = "Error: servlet unable to read test results file";
+		
+		//assign result set as cucumber's html output!
+		try {
+			result = readFile("C:\\index.html", StandardCharsets.UTF_8);
+		} catch (IOException e) {
+			System.out.println("Exception: file not found (Servlet.RequestHelper.readFile()");
+			e.printStackTrace();
+			System.out.println("End of Exception: file not found (Servlet.RequestHelper.readFile()");
+		}
+		/////////////////////////////////////////////////////
+		System.out.println("Returning html response to frontend:");
+		return result;
 	}
 
 }
