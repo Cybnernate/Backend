ee58c589c7849c6c092e77ae273fe2bce470789b
Linn <linncapellan@gmail.com>
2018-08-18 22:35:42 -0500

    3rd Commit Added path to cucumber app

--- Project2Hib/src/main/java/com/revature/model/P2User.java
+++ Project2Hib/src/main/java/com/revature/model/P2User.java
@@ -9,6 +9,22 @@ public class P2User {
 		this.password = password;
 	}
 	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	@Override
 	public String toString() {
 		return "P2User [username=" + username + ", password=" + password + "]";
