0d9b3237587d2879546e161dd61bd7d73c9d55af
hexagonlabs <h.simon93@gmail.com>
2018-08-19 15:20:56 -0400

    Update RequestHelper.java

--- project2BackEnd/src/main/java/com/revature/servlet/RequestHelper.java
+++ project2BackEnd/src/main/java/com/revature/servlet/RequestHelper.java
@@ -6,7 +6,22 @@ public class RequestHelper {
 
 	public static CharSequence process(HttpServletRequest request) {
 		System.out.println("boo");
-		return "hello";
+		return "
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Cucumber Features</title>
+    <link href="style.css" rel="stylesheet">
+    <script src="jquery-1.8.2.min.js"></script>
+    <script src="formatter.js"></script>
+    <script src="report.js"></script>
+  </head>
+  <body>
+    <div class="cucumber-report"></div>
+  </body>
+</html>
+";
 	}
 
 }
