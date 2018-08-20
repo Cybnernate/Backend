c662260b9651cbc9fccb8195a72be8e6e67b3ebf
hexagonlabs <h.simon93@gmail.com>
2018-08-20 08:35:57 -0400

    Update TestRunner.java

--- project2BackEnd/src/main/java/com/revature/cukes/TestRunner.java
+++ project2BackEnd/src/main/java/com/revature/cukes/TestRunner.java
@@ -3,7 +3,7 @@ package com.revature.cukes;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(plugin= {"pretty", "json:C:\\test.json", "html:C:\Program Files\Apache Software Foundation\Tomcat 8.5_Tomcat8-copy\webapps"}, features= {"src/main/java/com/revature/features"})
+@CucumberOptions(plugin= {"pretty", "json:C:\\test.json", "html:C:\\Program Files\Apache Software Foundation\Tomcat 8.5_Tomcat8-copy\webapps"}, features= {"src/main/java/com/revature/features"})
 public class TestRunner extends AbstractTestNGCucumberTests{
 
 }
