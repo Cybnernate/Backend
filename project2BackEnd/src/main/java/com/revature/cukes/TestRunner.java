4bd532a23c55a3a036e23cefade13d85f4320de4
hexagonlabs <h.simon93@gmail.com>
2018-08-19 22:04:51 -0400

    Revert "added frankin code to project2be and the build works"

--- project2BackEnd/src/main/java/com/revature/cukes/TestRunner.java
+++
@@ -1,9 +0,0 @@
-package com.revature.cukes;
-
-import cucumber.api.CucumberOptions;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
-
-@CucumberOptions(plugin= {"pretty", "html:C:\\temp\\html", "json:C:\\temp\\jsons"}, features= {"src/main/java/com/revature/features"})
-public class TestRunner extends AbstractTestNGCucumberTests{
-
-}
--- project2BackEnd/src/main/java/com/revature/features/verifyBatchesPage.feature
+++
@@ -1,8 +0,0 @@
-
-
-Feature: The user can view the batch information page
-
-  Scenario Outline: The user can view the batch information page
-    Given The user is logged in
-    When the user clicks the batches navbutton
-    Then the user arrives at the batch info page
